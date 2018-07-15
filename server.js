60463e21a0c2e33ffc32aeb298b970e66240b950
byhoson <bhson99@gmail.com>
2018-07-16 03:17:31 +0900

    add the add

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
@@ -10,4 +12,10 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
+})
+
+app.post('/add', (req, res) => {
+    res.render('index', { what: req.body.what })
+    //console.log(req.body.what)
+    //res.redirect('/')
 })
