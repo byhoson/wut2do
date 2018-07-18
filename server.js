db71d8803b352ad014b9e7984dd815d1f9324a2a
byhoson <bhson99@gmail.com>
2018-07-18 17:04:56 +0900

    add detail page for each todos

--- server.js
+++ server.js
@@ -20,13 +20,6 @@ MongoClient.connect(`mongodb://user1:${pw}@ds137601.mlab.com:37601/wut2do`, { us
     })
 })
 
-/*
-db.collection('todos').insertOne({ test: 'hello' }, (err, result) => {
-            if (err) return console.log(err)
-            console.log('saved to database')
-        })
-*/
-
 
 app.get('/', (req, res) => {
     db.collection('todos').find().toArray((err, todos) => {
@@ -36,18 +29,22 @@ app.get('/', (req, res) => {
     })
 })
 
+/********************************************/
 app.get('/todo/:_id', (req, res) => {
 
     let _id = req.params._id
     db.collection('todos').findOne({ _id: ObjectId(_id) }, (err, todo) => {
         if (err) return console.log(err)
-        console.log("success!")
+        res.render('todo.ejs', {
+            what: todo.what,
+            due: todo.due,
+            des: todo.des
+        })
     })
-
-    res.send(_id)
 
 
 })
+/********************************************/
 
 app.post('/add', (req, res) => {
     db.collection('todos').insertOne({
@@ -59,6 +56,5 @@ app.post('/add', (req, res) => {
         if (err) return console.log(err)
         console.log('saved to database')
     })
-    //console.log(req.body)
     res.redirect('/')
 })
